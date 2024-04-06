32d5fd19b9b1dc3b4a395b28eed9a729d672fc76
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2024-04-06 10:51:20 +0530

    Update 3_register.py

--- btd/pages/3_register.py
+++ btd/pages/3_register.py
@@ -6,9 +6,9 @@ import pymysql
 
 connection = pymysql.connect(
     host="sql6.freesqldatabase.com",
-    user="sql6694639",
-    password="3UN5p1Przn",
-    database="sql6694639"
+    user="sql6697115",
+    password="cDHQb51ErU",
+    database="sql6697115"
 )
 
 def db(email,username,password):
