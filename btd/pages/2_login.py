f890479f8c488fff072e38e15d99ee440976b4b7
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2024-04-19 12:22:05 +0530

    Update 2_login.py

--- btd/pages/2_login.py
+++ btd/pages/2_login.py
@@ -5,9 +5,9 @@ import os
 
 connection = pymysql.connect(
     host="sql6.freesqldatabase.com",
-    user="sql6697115",
-    password="cDHQb51ErU",
-    database="sql6697115"
+    user="sql6700287",
+    password="PFjqFdVb3x",
+    database="sql6700287"
 )
 
 def css():
